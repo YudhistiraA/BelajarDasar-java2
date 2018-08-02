c75a3e51039086386d294d1b8dd7d67dd4537627
Arya Rajasa <Arya Rajasa@DESKTOP-U37PTMA>
2018-08-02 08:18:54 +0700

    for-while-do while 02

---
+++ src/c2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arya Rajasa
+ */
+public class c2 {
+    public static void main (String[]args){
+        int x;
+        
+                
+        for(x=1;x<=10;x++){
+            System.out.print(x + " ");
+        }
+      //while
+      x=1;
+      while(x<=10){
+          System.out.print(x + " ");x++;
+      }
+        //do while
+        x=1;
+        do{System.out.print(x + " ");x++;
+        
+        }while(x<=10);
+                
+    }
+    
+}
