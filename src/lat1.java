1550b9af27327c0532ee3c861aadef8e53c55938
Arya Rajasa <Arya Rajasa@DESKTOP-U37PTMA>
2018-08-02 08:01:29 +0700

    for-while-do while

---
+++ src/lat1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arya Rajasa
+ */
+public class lat1 {
+    public static void main(String[]args){
+        //perulangan for
+        int y ;
+        for(y = 1;y<=5;y++){
+            System.out.println(" SMK MOKLET ");
+            
+        }
+        //perulangan do while
+        int i = 1 ;
+        while (i <= 5){
+            System.out.println("SMK jva");
+            i++;
+        }
+        //perulangan do while
+        int j = 1;
+                do{System.out.println("smk hadi");
+                j++;
+                }while (j<=5);
+                
+    }
+}
